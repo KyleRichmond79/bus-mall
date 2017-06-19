f3d0a7dd50e8cc49b3d73528e9bb2f22520a2416
Kyle Richmond <kylerichmond79@gmail.com>
2017-06-19 16:11:41 -0700

    constructor funtion created and pushed to allImages array.

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var allImgs = [];
+var Prod = [];
+
+function TestProd (name, path) {
+  this.name = name;
+  this.path = path;
+  this.tally = 0;
+  this.clicks = 0;
+  allImgs.push(this);
+}
+
+new TestProd ('bag', 'imgs/bag.jpg');
+new TestProd ('banana', 'imgs/banana.jpg');
+new TestProd ('bathroom', 'imgs/bathroom.jpg');
+new TestProd ('boots', 'imgs/boots.jpg');
+new TestProd ('breakfast', 'imgs/breakfast.jpg');
+new TestProd ('bubblegum', 'imgs/bubblegum.jpg');
+new TestProd ('chair', 'imgs/chair.jpg');
+new TestProd ('cthulhu', 'imgs/cthulhu.jpg');
+new TestProd ('dog-duck', 'imgs/dog-duck.jpg');
+new TestProd ('dragon', 'imgs/dragon.jpg');
+new TestProd ('pen', 'imgs/pen.jpg');
+new TestProd ('pet-sweep', 'imgs/pet-sweep.jpg');
+new TestProd ('scissors', 'imgs/scissors.jpg');
+new TestProd ('shark', 'imgs/shark.jpg');
+new TestProd ('sweep', 'imgs/sweep.png');
+new TestProd ('tauntaun', 'imgs/tauntaun.jpg');
+new TestProd ('unicorn', 'imgs/unicorn.jpg');
+new TestProd ('usb', 'usb.gif');
+new TestProd ('water-can', 'imgs/water-can.jpg');
+new TestProd ('wine-glass', 'imgs/wine-glass.jpg');
